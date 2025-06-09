39c97d8c8bee36fd6e8f4245c6caad131cf58015
niten <niten@fudo.org>
2025-06-09 13:44:22 -0700

    Forget 'unstable' aider for now.

--- users/common/niten-doom-emacs.nix
+++ users/common/niten-doom-emacs.nix
@@ -15,7 +15,7 @@ let
   emacsDeps = with pkgs;
     [
       (aspellWithDicts (ds: with ds; [ en en-computers en-science ]))
-      pkgs.pkgsUnstable.aider-chat
+      aider-chat
       bashInteractive
       clojure
       clojure-lsp
