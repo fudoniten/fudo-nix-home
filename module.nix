8a0e6044a3424bd2c54cb867a7dfe9dcc943cdc1
niten <niten@fudo.org>
2025-06-10 17:45:11 -0700

    Enable global pkgs

--- module.nix
+++ module.nix
@@ -89,6 +89,7 @@ in {
 
   config = mkIf cfg.enable {
     home-manager = {
+      useGlobalPkgs = true;
       users = listToAttrs (map ({ username, ... }@opts:
         nameValuePair username
         (import ./users/${getConfigUser opts}.nix inputs opts
