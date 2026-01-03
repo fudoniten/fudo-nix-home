2c5fe4f86f073d73ec0144c393d354e06c14ff0d
niten <niten@fudo.org>
2026-01-03 15:54:12 -0800

    fcitx5-chinese-addons is in qt6Packages for some reason

--- users/xiaoxuan.nix
+++ users/xiaoxuan.nix
@@ -66,7 +66,7 @@ else
         enable = true;
         type = "fcitx5";
         fcitx5.addons = with pkgs; [
-          fcitx5-chinese-addons
+          qt6Packages.fcitx5-chinese-addons
           fcitx5-gtk
           fcitx5-rime
         ];
