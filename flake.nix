ed19da15b6b79535fb2eb8373bc72ed67cba22ad
niten <niten@fudo.org>
2026-01-24 07:44:28 -0800

    Update to 25.11

--- flake.nix
+++ flake.nix
@@ -16,10 +16,10 @@
   description = "Fudo Home Manager Configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     doom-emacs = {
@@ -31,11 +31,11 @@
       flake = false;
     };
     fudo-pkgs = {
-      url = "github:fudoniten/fudo-nix-pkgs/25.05";
+      url = "github:fudoniten/fudo-nix-pkgs/25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:danth/stylix/release-25.05";
+      url = "github:danth/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     polymuse = {
